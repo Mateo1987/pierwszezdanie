ec6f7b9b0d8889de19657a31dc56f59da5e7e011
halas <piotr.halasiewicz@hadora.pl>
2015-11-06 23:16:21 +0100

    działa parsowanie adresu xml ze strony www utworu, do zrobienia wybór pierwszego zdania, nie akapitu, skomentowanie kodu

--- index.php
+++ index.php
@@ -18,20 +18,17 @@
     $normal_url =  $novels[$random_book];
     echo $normal_url;
     $html = file_get_html($normal_url);
-    $xml_relative = $html->find('div.other-tools ul li');
-    $comma_separated = implode(",", $xml_relative);
-    $xml_relative = substr($str, 0, strpos($str, '_'));
-    echo $comma_separated;
-    echo $xml_relative[0];
-/*    $xml_url = "http://wolnelektury.pl/media/book/xml/$title_no_dash.xml";
-    echo $xml_url;
-    $chosen_book = simplexml_load_file($xml_url);
+    $links_div = $html->find('div.other-tools ul li a[href*=xml]');
+    $comma_separated = implode(",", $links_div);
+    $xml_relative = substr($comma_separated,9,-26);
+    $xml_final = "http://wolnelektury.pl$xml_relative";
+    $chosen_book = simplexml_load_file($xml_final);
     $author = $chosen_book->powiesc->autor_utworu;
     $title = $chosen_book->powiesc->nazwa_utworu;
     $sentence = $chosen_book->powiesc->akap[0];
     echo "<p>Pierwsze zdanie:$sentence</p>";
     echo "<p>autor:$author</p>";
-    echo "<p>tytuł:$title</p>";*/
+    echo "<p>tytuł:$title</p>";
    
 ?>
 </div>
