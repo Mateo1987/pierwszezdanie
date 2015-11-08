52cd6c9218a32092249f607d0de0ac53e79a2b9e
halas <piotr.halasiewicz@hadora.pl>
2015-11-08 14:05:40 +0100

    Poprawione linki do css i skryptów

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
  <head>
   <title>Pierwsze zdanie losowej powieśi z wolnych lektur - beta</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <link rel="stylesheet" href="style.css">
-  <script type="text/javascript" src="scripts.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <script type="text/javascript" src="scripts/scripts.js"></script>
  </head>
  <body>
 <div class="invisible">
---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var test = "test";
+    console.log("Cześć");
+    var cover = $('dc\\:relation\\.coverImage\\.source');
+    console.log(cover);
+    $('#tutaj').append(cover);
+
+});
