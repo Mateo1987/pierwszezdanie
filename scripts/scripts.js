890f0a0c51cb769b395d75e34abc3b8f22868934
halas <piotr.halasiewicz@hadora.pl>
2015-11-15 22:29:49 +0100

    Formatowanie tekstów na canvas, dodano stały obrazek tła na tę chwilę

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,45 @@
 $(document).ready(function() {
 	var book_credits = title + ", "+author;
+	// tymczasowo wrzucona na sztywno okładka
+	var image_credits = "Obrazek tła: davebloggs007@Flickr CC BY 2.0"
 	var canvas = document.getElementById("main-canvas");
+	var imageObj = new Image();
+
+	imageObj.onload = function() {
+		context.drawImage(imageObj, 0, 0);
+		CanvasTextWrapper(canvas, sentence,{
+			textAlign: "center",
+			font: "40pt Arial",
+			lineHeight: 3,
+			verticalAlign: "middle",
+			strokeText: true,
+			paddingX: 10,
+			paddingY: 20
+		});   
+		CanvasTextWrapper(canvas, book_credits,{
+			textAlign: "left",
+			font: "italic 20pt Arial",
+			lineHeight: 3,
+			verticalAlign: "bottom",
+			paddingX: 50,
+			paddingY: 50
+		});
+		CanvasTextWrapper(canvas, image_credits,{
+			textAlign: "left",
+			font: "italic 10pt Arial",
+			lineHeight: 3,
+			verticalAlign: "bottom",
+			paddingX: 50,
+			paddingY: 5
+		});
+	};
+	imageObj.src = './img/4689.jpg';
 	canvas.width = 1000;
 	canvas.height = 600;
 	context = canvas.getContext("2d");
 	context.lineWidth = 1;
 	context.strokeStyle = "black";
 	context.fillStyle = 'white';
-	CanvasTextWrapper(canvas, sentence,{
-		textAlign: "center",
-		font: "40pt Arial",
-		lineHeight: 3,
-		verticalAlign: "middle",
-		strokeText: true,
-		paddingX: 10,
-		paddingY: 20
-	});   
-	CanvasTextWrapper(canvas, book_credits,{
-		textAlign: "left",
-		font: "italic 20pt Arial",
-		lineHeight: 3,
-		verticalAlign: "bottom",
-		strokeText: true,
-		paddingX: 50,
-		paddingY: 20
-	});   
+	
+
 });
